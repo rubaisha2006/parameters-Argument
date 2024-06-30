b8546e893df6bde25812040aa9c74badb74ceee2
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:44:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function Fahad(masseage) {
+    return masseage;
+}
+// Arguments
+var call = Fahad("I like Typescript");
+console.log(call);
+function problem(num1, num2) {
+    return num1 + num2;
+}
+var calling = problem(7, 8);
+console.log(calling);
---
+++ main.ts
@@ -0,0 +1,12 @@
+function Fahad(masseage:String){
+    return masseage;
+}
+// Arguments
+let call = Fahad("I like Typescript");
+console.log(call);
+
+function problem(num1:number , num2:number){
+    return num1 + num2;
+}
+let calling = problem(7 , 8);
+console.log(calling);
